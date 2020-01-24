03a692a77521e45061592adae63e3280d26d997e
Kyaw Soe Naing <kyawsoenaing@my.jcu.edu.au>
2020-01-24 18:32:53 +0800

    add class movietowatch

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 """
-Name: Kyaw Soe Naing
-Date: 17/1/2020
+Name:Kyaw Soe Naing
+Date: 24 Jan 2020
 Brief Project Description:
-GitHub URL: https://github.com/Ezra2952/assignment-02-Ezra2952
+GitHub URL:
 """
 # TODO: Create your main program in this file, using the MoviesToWatchApp class
-
+from kivy.app import StringProperty
+from kivy.properties import ListProperty
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.button import Button
+from movie import Movie
+from moviecollection import MovieCollection
+
+SORTING_KEY = {"Category": "category", "Title": "title", "Year": "year", "Watched": "is_watched"}
+CATEGORY_LIST = ["Action", "Comedy", "Documentary", "Drama", "Fantasy", "Thriller"]
 
 
 class MoviesToWatchApp(App):
-    """..."""
-    pass
+    """MoviesToWatchApp(App) with Kivy APP following movies """
+    current_spinner = StringProperty()
+    message = StringProperty()
+    watched_status = StringProperty()
+    sorting_keys = ListProperty()
 
 
-if __name__ == '__main__':
-    MoviesToWatchApp().run()
+MoviesToWatchApp().run()
